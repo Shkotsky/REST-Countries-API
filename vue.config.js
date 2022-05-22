3936765b0f71cb3e6bcb45c1ff1ce74ad8c8e887
Marko Hristovski <hristovski1407@gmail.com>
2022-05-23 00:01:06 +0200

    cleaning up code

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
-
-// module.exports = {
-//   css: {
-//     loaderOptions: {
-//       scss: {
-//         additionalData: `@import "@/assets/_variables.scss";` `@import "@/assets/_main.scss"; `
-//       }
-//     }
-//   }
-// }
